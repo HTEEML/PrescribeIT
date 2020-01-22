5d9f9764a0c6a78fcb2b71297579064d8a772921
Jarif Khan <jarifjak@gmail.com>
2020-01-22 16:21:19 +0600

    added bottom navigation

---
+++ app/src/main/java/com/jarifjak/prescribeit/activity/MainActivity.java
@@ -0,0 +1,58 @@
+package com.jarifjak.prescribeit.activity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.jarifjak.prescribeit.Fragment.DoctorFragment;
+import com.jarifjak.prescribeit.Fragment.MedicalFileFragment;
+import com.jarifjak.prescribeit.Fragment.SettingsFragment;
+import com.jarifjak.prescribeit.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView bottomNavigation;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        bottomNavigation.setOnNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment selectedFragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.nav_doctor:
+                selectedFragment = new DoctorFragment();
+                break;
+            case R.id.nav_medical_file:
+                selectedFragment = new MedicalFileFragment();
+                break;
+            case R.id.nav_settings:
+                selectedFragment = new SettingsFragment();
+                break;
+        }
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                selectedFragment,null).commit();
+
+        return true;
+
+    }
+}
